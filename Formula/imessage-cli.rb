bd010206162857ccfe9af48e326ba8481d23d8fa
batuhan içöz <batuhan@icoz.co>
2026-05-07 14:11:01 +0200

    Add imessage-cli formula

--- Formula/imessage-cli.rb
+++ Formula/imessage-cli.rb
@@ -1,20 +1,17 @@
 class ImessageCli < Formula
   desc "Automate iMessage locally on your Mac"
   homepage "https://github.com/beeper/platform-imessage"
-  url "https://github.com/beeper/platform-imessage/archive/refs/tags/v0.21.1.tar.gz"
-  sha256 "d7d3964b337b13064ec571d22f888bf748719148f62f40eb48facef4b4450bf1"
+  url "https://github.com/beeper/platform-imessage/releases/download/v0.21.1/imessage-cli-0.21.1-macos-universal.tar.gz"
+  sha256 "4bbbdbd122d65d1c0c73e7c567d270e183b73c3af80f4f38cd23ebfe622bc631"
   license "MIT"
   head "https://github.com/beeper/platform-imessage.git", branch: "main"
-
-  depends_on xcode: ["26.1", :build]
 
   on_macos do
     depends_on macos: :big_sur
   end
 
   def install
-    system "swift", "build", "-c", "release", "--product", "imessage-cli", "--disable-sandbox"
-    bin.install ".build/release/imessage-cli"
+    bin.install "imessage-cli"
   end
 
   test do
