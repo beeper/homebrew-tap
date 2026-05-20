e149b84d18b616204a9df6cf725225a9fe22bbcd
batuhan içöz <batuhan@icoz.co>
2026-05-20 03:32:43 +0200

    Fix cli formula URL selection

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -4,14 +4,14 @@ class Cli < Formula
   license "MIT"
   version "0.6.2"
 
-  on_arm do
+  if OS.mac? && RbConfig::CONFIG["host_cpu"] == "arm64"
     url "https://github.com/beeper/cli/releases/download/v0.6.2/beeper-cli-0.6.2-macos-arm64.zip"
     sha256 "688ccde7e7d044d33980cd06474bf1ae7215ccf8ca79967262fa3bfb85a2589a"
-  end
-
-  on_intel do
+  elsif OS.mac? && RbConfig::CONFIG["host_cpu"] == "x86_64"
     url "https://github.com/beeper/cli/releases/download/v0.6.2/beeper-cli-0.6.2-macos-x64.zip"
     sha256 "4113a1979cfbd7839f14743158e70c12efa941313afb77ab2b11a08309196186"
+  else
+    odie "Unsupported platform"
   end
 
   def install
