3c55798c976a435be90088af849f9bb1ce902c51
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2026-05-24 02:58:06 +0530

    Bump imessage-cli to 0.22.8

--- Formula/imessage-cli.rb
+++ Formula/imessage-cli.rb
@@ -1,8 +1,8 @@
 class ImessageCli < Formula
   desc "Automate iMessage locally on your Mac"
   homepage "https://github.com/beeper/platform-imessage"
-  url "https://github.com/beeper/platform-imessage/releases/download/v0.22.5/imessage-cli-0.22.5-macos-universal.tar.gz"
-  sha256 "4322e1f5a5a6f48272c68c0aa4e1289937044d04889b2b968387d478ea578c63"
+  url "https://github.com/beeper/platform-imessage/releases/download/v0.22.8/imessage-cli-0.22.8-macos-universal.tar.gz"
+  sha256 "a79bf487ef50939a6d75917901700a260d0b103096641ce56b18af5e80e6f33d"
   license "MIT"
   head "https://github.com/beeper/platform-imessage.git", branch: "main"
 
